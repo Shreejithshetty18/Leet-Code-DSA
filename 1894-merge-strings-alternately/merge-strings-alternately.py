0d4f6abdd8a72f31a841aac0316063d34dec976f
Shreejith Shetty <shettyshreejith66@gmail.com>
2026-07-01 22:22:23 +0530

    Time: 50 ms (23.81%) | Memory: 19.3 MB (55.40%) - LeetSync

---
+++ 1894-merge-strings-alternately/merge-strings-alternately.py
@@ -0,0 +1,30 @@
+class Solution:
+    def mergeAlternately(self, a: str, b: str) -> str:
+        c=""
+        i=0
+        j=0
+        pick=0
+        
+        while i<=len(a)-1 and j<=len(b)-1:
+            if(pick==0):
+                c=c+a[i]
+                i=i+1
+                pick=1
+            else:
+                c=c+b[j]
+                j=j+1
+                pick=0
+
+        while i<=len(a)-1:
+            c=c+a[i]
+            i=i+1
+
+        while j<=len(b)-1:
+            c=c+b[j]
+            j=j+1
+        return c
+
+
+
+
+
