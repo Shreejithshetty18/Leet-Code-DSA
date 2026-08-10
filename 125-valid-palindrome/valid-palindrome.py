e938fbef3bfa4669f0f460e419475120fce8e656
Shreejith Shetty <shettyshreejith66@gmail.com>
2026-08-10 23:21:04 +0530

    Time: 7 ms (81.11%) | Memory: 19.5 MB (76.76%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        new=""
+        for i in s:
+            if i.isalnum():
+                new+=i.lower()
+
+        i=0
+        j=len(new)-1
+        while(i<j):
+            if(new[i]!=new[j]):
+                return False
+            i+=1
+            j-=1
+        return True
+        
