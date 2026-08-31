b5142c370927ae81f92439c636aa29e0da9fac66
Shreejith Shetty <shettyshreejith66@gmail.com>
2026-08-31 21:23:10 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (55.10%) - LeetSync

--- 9-palindrome-number/palindrome-number.py
+++ 9-palindrome-number/palindrome-number.py
@@ -4,10 +4,8 @@ class Solution:
         i=0
         j=len(s)-1
         while(i<j):
-            if s[i]!=s[j]:
+            if(s[i]!=s[j]):
                 return False
             i+=1
             j-=1
-            
-           
         return True
