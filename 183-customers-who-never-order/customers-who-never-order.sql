058dee373dffd05e918c81f605ab1e986943f1ee
Shreejith Shetty <shettyshreejith66@gmail.com>
2026-08-31 22:12:58 +0530

    Time: 543 ms (88.38%) | Memory: 0B (100.00%) - LeetSync

--- 183-customers-who-never-order/customers-who-never-order.sql
+++ 183-customers-who-never-order/customers-who-never-order.sql
@@ -1,7 +1,7 @@
-/* Write your PL/SQL query statement below */
-SELECT name AS Customers
-FROM Customers
-WHERE id NOT IN (
-    SELECT customerId
-    FROM Orders
-);
+# Write your MySQL query statement below
+
+SELECT c.name AS Customers
+FROM Customers c
+LEFT JOIN Orders o
+ON c.id = o.customerId
+WHERE o.customerId IS NULL;
